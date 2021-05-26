be73ab7d048cb4c3e7e3ef8904a9b18d233c849b
Antonio Gonzalez <toni@wificlick.es>
2021-05-26 22:59:16 +0100

    Proyecto JPA EclipseLink 2.1 Mejoras 1.5

--- src/main/java/controlador/JuegoController.java
+++ src/main/java/controlador/JuegoController.java
@@ -31,7 +31,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
